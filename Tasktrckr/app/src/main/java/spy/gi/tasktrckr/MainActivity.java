715cc76ecd6f5539140c75f158468c3a2b00ab28
spygi <spygiannakis@hotmail.com>
2016-04-10 21:53:13 +0200

    started a new project with a "blank activity" with a fragment in it

--- Tasktrckr/app/src/main/java/spy/gi/tasktrckr/MainActivity.java
+++ Tasktrckr/app/src/main/java/spy/gi/tasktrckr/MainActivity.java
@@ -5,7 +5,6 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -14,7 +13,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Log.d(MainActivity.class.getSimpleName(), "asdasdas");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
