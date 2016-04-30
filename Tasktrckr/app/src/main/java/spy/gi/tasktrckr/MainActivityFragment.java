60419eb8f4dd0eb5d2399de46f45cb011d008a38
spygi <spygiannakis@hotmail.com>
2016-04-30 16:22:07 +0200

    change icon to "+" and have a list fragment with static data next: make the list of data dynamic

--- Tasktrckr/app/src/main/java/spy/gi/tasktrckr/MainActivityFragment.java
+++ Tasktrckr/app/src/main/java/spy/gi/tasktrckr/MainActivityFragment.java
@@ -1,15 +1,17 @@
 package spy.gi.tasktrckr;
 
+import android.app.ListFragment;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MainActivityFragment extends Fragment {
+public class MainActivityFragment extends ListFragment {
 
     public MainActivityFragment() {
     }
@@ -17,6 +19,20 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        return inflater.inflate(R.layout.task_list, container, false);
     }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(getActivity(), R.array.Planets, android.R.layout.simple_list_item_1);
+        setListAdapter(adapter);
+        // getListView().setOnItemClickListener(this);
+    }
+
+//    @Override
+//    public void onItemClick(AdapterView<?> parent, View view, int position,long id) {
+//        Toast.makeText(getActivity(), "Item: " + position, Toast.LENGTH_SHORT).show();
+//    }
+
 }
