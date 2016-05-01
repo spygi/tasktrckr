5b6d2d31e2cd2ce3d27b942fcee11d17a34910e1
spygi <spygiannakis@hotmail.com>
2016-05-01 18:26:36 +0200

    on click show new activity (to insert task)

---
+++ Tasktrckr/app/src/main/java/spy/gi/tasktrckr/InsertTaskActivity.java
@@ -0,0 +1,14 @@
+package spy.gi.tasktrckr;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class InsertTaskActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.insert_task);
+    }
+}
